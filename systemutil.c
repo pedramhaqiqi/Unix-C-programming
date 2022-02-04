ea110387c7f6a9f37dc5b5bfff4b8dbed63c070b
Pedram Meskoubhaghighi <meskoubh@iits-b473-01.utsc-labs.utoronto.ca>
2022-02-03 22:55:48 -0500

    finalized System utilization Monitor

--- systemutil.c
+++ systemutil.c
@@ -90,7 +90,7 @@ void print_memory_usage(MemoryValue memory) {
   double total_swap = memory.totalswap;
   double free_swap = memory.freeswap;
 
-  printf("%0.2lf GB / %0.2lf GB  -- %0.2lf GB / %0.2lf GB\n",
+  printf("%0.2lf GB / %0.2lf GB  -- %0.2lf GB / %0.2lf GB \t",
          total_ram - free_ram, total_ram,
          total_ram + total_swap - free_swap - free_ram, total_ram + total_swap);
 }
@@ -128,11 +128,49 @@ void print_cpu_usage(CpuValues cpu_current1, CpuValues cpu_current2) {
   unsigned long long effective_total_change =
       effective_total_2 - effective_total_1;
   unsigned long long total_change = total_2 - total_1;
-  printf("Total cpu usage: %0.2lf %%\n ",
-         100 * (double)effective_total_change / (double)total_change);
+  
+  double usage = 100 * (double)effective_total_change / (double)total_change;
+
+  printf("Total cpu usage: %0.2lf %%\n", usage);
+  
+   
 }
 
-void memory_usage_change(MemoryValue memory1, MemoryValue memory2) {
+
+void cpu_graphics(CpuValues cpu_current1, CpuValues cpu_current2){
+
+  unsigned long long total_1 = cpu_current1.user + cpu_current1.nice +
+                               cpu_current1.system + cpu_current1.idle +
+                               cpu_current1.iowait + cpu_current1.irq +
+                               cpu_current1.softirq;
+
+  /*delayed cpu sample*/
+  unsigned long long total_2 = cpu_current2.user + cpu_current2.nice +
+                               cpu_current2.system + cpu_current2.idle +
+                               cpu_current2.iowait + cpu_current2.irq +
+                               cpu_current2.softirq;
+  /*change calculation*/
+  unsigned long long effective_total_1 = total_1 - cpu_current1.idle;
+  unsigned long long effective_total_2 = total_2 - cpu_current2.idle;
+
+  unsigned long long effective_total_change =
+      effective_total_2 - effective_total_1;
+  unsigned long long total_change = total_2 - total_1;
+  
+  double percentage = 100 * (double)effective_total_change / (double)total_change;
+  int increment = (int)percentage;
+
+  printf("\t");
+  for (int i = 0; i <= increment; i++ )
+  {
+    printf("|");
+  }
+    printf(" %0.2lf", percentage);
+    printf("\n");
+  }
+
+void memory_graphics(MemoryValue memory1, MemoryValue memory2){
+  
   /*the base sample*/
   double Used_ram_1 = memory1.totalram - memory1.freeram;
   double Used_swap_1 = memory1.totalswap - memory1.freeswap;
@@ -141,18 +179,38 @@ void memory_usage_change(MemoryValue memory1, MemoryValue memory2) {
   double Used_ram_2 = memory2.totalram - memory2.freeram;
   double Used_swap_2 = memory2.totalswap - memory2.freeswap;
 
-  double difference = (Used_swap_2 + Used_ram_2 - Used_swap_1 - Used_ram_1);
-  if (difference < 0) {
-    printf("memory change is 0.00 GB\n");
+  double change = (Used_swap_2 + Used_ram_2 - Used_swap_1 - Used_ram_1);
+
+  int increment = (int)(change/0.001);
+  int absincr = abs(increment);
+
+  if (change < 0){
+    for (int i = 0; i <= absincr; i++){
+      printf(":");
+    }
+    printf("@ ");
+    printf("%0.3lf change", change);
+    printf("\n");
   } else {
-    printf("memory change is %0.5lf GB\n", difference);
+    for (int i = 0; i <= absincr; i++){
+      printf("#");
+    }
+    printf("* ");
+    printf("%0.3lf change", change);
+    printf("\n");
+
   }
 }
-
-void clear_line()
-{
-    printf("\e[2K");
+int abs(int value){
+  int final;
+  if (value < 0){
+    final = -1 * value;
+  } else{
+    final = value;
+  }
+  return final;
 }
+
 
 void print_newline(int lines){
   for (int i = 0; i < lines; i++){
@@ -166,13 +224,17 @@ int *cmd_parsing_function(int argc, char *argv[]) {
   int *flag_arr;
   int arg_given = 0;
   flag_arr = (int *)calloc(7, sizeof(int));
-  flag_arr[5] = 1; //Default value for time delay built in
-  flag_arr[6] = 10;////Default value for sample size built in
+  flag_arr[5] = 10; //Default value for time delay built in
+  flag_arr[6] = 1;////Default value for sample size built in
 
   while ((opt = getopt_long(argc, argv, "g", long_options, &option_index)) !=
          -1) {
     /* Detect the end of the options. */
     switch (opt) {
+
+      case 'g':
+        flag_arr[2] = 1;
+        break;
       case 1:
         flag_arr[0] = 1;
         break;
@@ -192,7 +254,7 @@ int *cmd_parsing_function(int argc, char *argv[]) {
           arg_given = 1;
           break;
         } else {
-          printf("ERROR: --samples argument must be a positive integer, entered '%s'\n", optarg);
+          printf("ERROR: --samples argument must be only a positive integer, entered '%s'\n", optarg);
           exit(1);
         }
       case 5:
@@ -202,7 +264,7 @@ int *cmd_parsing_function(int argc, char *argv[]) {
           arg_given = 1;
           break;
         } else {
-          printf("ERROR: --tdelay argument must be a positive integer %s\n", optarg);
+          printf("ERROR: --tdelay argument argument must be only a positive integer, entered %s\n", optarg);
           exit(1);
         }
       case '?':
@@ -218,7 +280,7 @@ int *cmd_parsing_function(int argc, char *argv[]) {
       default:
         printf("%s",
                "ERROR in identifiying command line command, pass read user "
-               "manual for usage");
+               "manual for usage\n");
         exit(1);
     }
   }
@@ -230,19 +292,19 @@ int *cmd_parsing_function(int argc, char *argv[]) {
     if ((strtol(argv[temp++], NULL, 10) != 0) && arg_given == 0) {
       flag_arr[5] = strtol(argv[optind++], NULL, 10);
     } else {
-          printf("ERROR: Positional arguments must be a positive integer\n");
+          printf("ERROR: Positional arguments not used properly, please refer to manual\n");
           exit(1);}
     if ((strtol(argv[temp++], NULL, 10) != 0)) {
       flag_arr[6] = strtol(argv[optind++], NULL, 10);
     } else {
-          printf("ERROR: Positional arguments must be a positive integer\n");
+          printf("ERROR: Positional arguments not used properly, please refer to manual\n");
           exit(1);}  
     }
      else if(optind < argc && argc - optind != 2){
       printf("%s", "ERROR: Incorrect Positinal arguments\n");
       exit(1);
     }
-
+  
   return flag_arr;
 }
 
@@ -261,8 +323,8 @@ int main(int argc, char **argv) {
 
 
   /*Set sample size and time delay of */
-  int sample_size = cmd_flags[6];
-  int time_delay = cmd_flags[5];
+  int sample_size = cmd_flags[5];
+  int time_delay = cmd_flags[6];
 
   /*Set flags into human friendly variables*/
   int sys = cmd_flags[0];
@@ -271,67 +333,137 @@ int main(int argc, char **argv) {
   int samp = cmd_flags[3]; 
   int tdlay = cmd_flags[4];
 
+  /* Arrays that will remember the past for us*/
+  CpuValues cpu_array[sample_size + 1]; // to Store n = sample_size samples and 1 base sample
+  MemoryValue memory_array[sample_size + 1];
+
   /* Base variables used for memory and cpu */
   curr_memory_sample = get_memory_usage();
   curr_cpu_sample = get_cpu_values();
 
-  /* Arrays that will remember the past for us*/
-  CpuValues cpu_array[sample_size];
-  MemoryValue memory_array[sample_size];
+  cpu_array[0] = curr_cpu_sample;
+  memory_array[0] = curr_memory_sample;
 
-  for (int i = 0; i <= sample_size; i++) {
+  for (int i = 0; i < sample_size; i++) {
     /*Terminal escape codes*/
     printf("\033[2J"); //Clear screen
     printf("\e[1;1H"); //position curser to 1,1 row and column
+
     /*delayed samples*/
     sleep(time_delay);
+
     next_memory_sample = get_memory_usage();
     next_cpu_sample = get_cpu_values();
-    
 
-    /*function calls*/
-    printf("### Memory ### (Phys.Used/Tot -- Virtual Used/Tot\n");
-    print_memory_usage(curr_memory_sample);
-    memory_usage_change(curr_memory_sample, next_memory_sample);
-    printf("---------------------------------------\n");
-    printf("### CPU information ###\n");
-    print_cpu_usage(curr_cpu_sample, next_cpu_sample);
-    printf("---------------------------------------\n");
-    printf("### System Information ###\n");
-    print_sysinfo();
-    printf("---------------------------------------\n");
-    printf("### Sessions/Users ###\n");
-    print_user_session();
-    
+    cpu_array[i+1] = next_cpu_sample;
+    memory_array[i+1] = next_memory_sample;
 
-    if (sys == 1){
+
+    if ((sys == 1 && uzer == 1) || (sys == 0 && uzer == 0)){
+      if (graf == 1){
+        printf("---------------------------------------\n");
+        printf("### Memory ### (Phys.Used/Tot -- Virtual Used/Tot\n");
+        for (int j = 0; j <= i ; j++){
+          fflush(stdout);
+          print_memory_usage(memory_array[j]); 
+          memory_graphics(memory_array[j], memory_array[j + 1]);
+        }
+        print_newline((sample_size-1) - i);
+        printf("---------------------------------------\n");
+        printf("### CPU information ###\n");
+        print_cpu_usage(cpu_array[i], cpu_array[i + 1]);
+        for (int j = 0; j <= i ; j++){
+            cpu_graphics(cpu_array[j], cpu_array[j + 1]);
+        }
+        print_newline((sample_size-1) - i); 
+        printf("---------------------------------------\n");
+        printf("### System Information ###\n");
+        print_sysinfo();
+        printf("---------------------------------------\n");
+        printf("### Sessions/Users ###\n");
+        print_user_session();
+        printf("---------------------------------------\n");
+      }
+      else{
+        printf("---------------------------------------\n");
+        printf("### Memory ### (Phys.Used/Tot -- Virtual Used/Tot\n");
+        for (int j = 0; j <= i ; j++){
+          fflush(stdout);
+          print_memory_usage(memory_array[j]);
+          printf("\n");
+        }
+        print_newline((sample_size-1) - i);
+        
+        printf("---------------------------------------\n");
+        printf("### CPU information ###\n");
+        print_cpu_usage(cpu_array[i], cpu_array[i + 1]);
+        printf("---------------------------------------\n");
+        printf("### System Information ###\n");
+        print_sysinfo();
+        printf("---------------------------------------\n");
+        printf("### Sessions/Users ###\n");
+        print_user_session();
+        printf("---------------------------------------\n");
+      }
+    } 
+    
+    else if (sys == 1){
+      if (graf == 1){
+        printf("---------------------------------------\n");
+        printf("### Memory ### (Phys.Used/Tot -- Virtual Used/Tot\n");
+        for (int j = 0; j <= i ; j++){
+          fflush(stdout);
+          print_memory_usage(memory_array[j]); 
+          memory_graphics(memory_array[j], memory_array[j + 1]);
+        }
+        print_newline((sample_size-1) - i);
+        printf("---------------------------------------\n");
+        printf("### CPU information ###\n");
+        print_cpu_usage(cpu_array[i], cpu_array[i + 1]);
+        for (int j = 0; j <= i ; j++){
+            cpu_graphics(cpu_array[j], cpu_array[j + 1]);
+        }
+        print_newline((sample_size-1) - i); 
+        printf("---------------------------------------\n");
+        printf("### System Information ###\n");
+        print_sysinfo();
+      }
+      else{
+        printf("---------------------------------------\n");
+        printf("### Memory ### (Phys.Used/Tot -- Virtual Used/Tot\n");
+        for (int j = 0; j <= i ; j++){
+          fflush(stdout);
+          print_memory_usage(memory_array[j]);
+          printf("\n");
+        }
+        print_newline((sample_size-1) - i);
+        printf("---------------------------------------\n");
+        printf("### CPU information ###\n");
+        print_cpu_usage(cpu_array[i], cpu_array[i + 1]);
+        printf("---------------------------------------\n");
+        printf("### System Information ###\n");
+        print_sysinfo();
+        printf("---------------------------------------\n");
+      }
       
+    } 
+    else if (uzer == 1){
+      if (graf == 1){
+        printf("Graphics flag cannot be passed for User only\n");
+        exit(1);
+      } else{
+        printf("---------------------------------------\n");
+      printf("### System Information ###\n");
+      print_sysinfo();
+      printf("---------------------------------------\n");
+      printf("### Sessions/Users ###\n");
+      print_user_session();
+      printf("---------------------------------------\n");
+      }
     }
+    fflush(stdout);
 
-    if (uzer == 1){
-
-    }
-
-    if (sys == 1 and uzer == 1){
-
-    }
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-    curr_cpu_sample = next_cpu_sample;
-    curr_memory_sample = next_memory_sample;
   }
+  
   exit(0);
 }
